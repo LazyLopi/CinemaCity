d2566c349b5555f306a805a81a9517790f4ed47b
unknown <konradek982@gmail.com>
2024-10-28 09:33:43 +0100

    small fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import FilmList from "./filmList.jsx";
+import Gora from "./gora.jsx";
 
 function App() {
   return (
     <div className="App">
+      <Gora />
       <FilmList />
     </div>
   );
